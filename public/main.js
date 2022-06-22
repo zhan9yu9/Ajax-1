821fd50df76f93a25a3b46ac4ea2509a2c3ca76d
sophieyu <995794386@qq.com>
2022-06-22 22:33:04 +0800

    补充代码

--- public/main.js
+++ public/main.js
@@ -1,12 +1,88 @@
-const request = new XMLHttpRequest()
-
-getCSS.onclick = () =>{
-    request.open('GET','/style.css');
-    request,onload = () =>{
-        console.log('成功了')
-    };
-    request.onerror = () =>{
-        console .logo('失败了')
+let n =1;
+getPage.onclick = () => {
+    const request = new XMLHttpRequest();
+    request.open('GET',`page${n+1}`);
+    request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200){
+        const array = JSON.parse(request.response);
+        array.forEach(item =>{
+            const li = document.createElement("li");
+            li.textContent = item.id;
+            xxx.appendChild(li);
+        });
+        n+=1
+    }
     };
     request.send();
+};
+
+getJSON.onclick = () =>{
+    const request = new XMLHttpRequest();
+    request.open('GET','/5.json');
+    request.onreadystatechange = () =>{
+        if(request.readyState === 4 && request.status === 200){
+        const object = JSON.parse(request.response)
+        myName.textContent = object.name
+        }
+    };
+    request.send();
+}
+
+
+getXML.onclick = () =>{
+    const request = new XMLHttpRequest();
+    request.open('GET',"/4.xml");
+    request.onreadystatechange = () =>{
+        if(request.readyState ===4 && request.status ===200){
+            const dom = request.responseXML;
+            const text = dom.getElementsByTagName("warning")[0].textContent;
+            console.log(text.trim());
+        }
+    };
+    request.send();
+}
+
+
+getHTML.onclick = () =>{
+    getHTML.onclick = () => {
+        const request = new XMLHttpRequest();
+        request.open("GET", "/3.html");
+        request.onload = () => {
+          const div = document.createElement('div');
+          div.innerHTML = request.response;
+          document.body.appendChild(div);
+        };
+        request.onerror = () =>{}
+        request.send();
+    }
+}
+
+getJS.onclick = () =>{
+    const request = new XMLHttpRequest()
+    request.open('GET',"/2.js");
+    request,onload = () =>{
+    const script = document.createElement('script')
+    script.innerHTML = request.response
+    document.body.appendChild(script)
+    };
+    request.onerror = () =>{};
+    request.send();
+}
+
+getCSS.onclick = () =>{
+    const request = new XMLHttpRequest()
+    request.open('GET',"/style.css");
+    request.onreadystatechange = () =>{
+        console.log(request.readyState);
+        if (request.readyState === 4) {
+            if (request.status >= 200 && request.status < 300) {
+                const style = document.createElement("style")
+                style.innerHTML = request.response;
+                document.head.appendChild(style);
+            } else {
+                alert("加载 CSS 失败");
+            }
+         }
+     };
+     request.send();
 }
